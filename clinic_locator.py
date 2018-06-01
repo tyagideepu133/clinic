a01c3bec661b3f26a07ae426975386d5d6a12744
deepanshut041 <deepanshut041@gmail.com>
2018-06-01 06:11:51 +0530

    Fix rrror handling and query string

--- clinic_locator.py
+++ clinic_locator.py
@@ -11,7 +11,6 @@ services_df = pd.read_csv(DEFAULT_FOLDER_LOCATION + "/services.csv")
 def find_clinic(service_id):
     if service_id == 0:
         clinics = cliniclocations_df.drop_duplicates('ClinicID', keep='first')
-        print(clinics)
     else:
         clinics = cliniclocations_df.loc[cliniclocations_df['ServiceID'] == service_id]
     clinics.drop(['ClinicID', 'ClinicServicesID', 'ServiceID'], axis=1, inplace=True)
@@ -20,8 +19,10 @@ def find_clinic(service_id):
 
 @app.route("/getclinics")
 def getclinics():
-    service_id = int(request.args.get('serviceid'))
-    clinics_dict = find_clinic(service_id)
+    service_id = request.args.get('serviceid')
+    if service_id == None:
+        return json.dumps({"error":"Service Id not found"})
+    clinics_dict = find_clinic(int(service_id))
     clinics_json = json.dumps(clinics_dict)
     return clinics_json
 
@@ -34,6 +35,10 @@ def getServices():
 @app.route("/")
 def homePage():
     return render_template('clinic_map.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
